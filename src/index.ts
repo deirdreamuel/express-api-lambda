2fe2d24d401c766bf05cb29010fb8aa380e59a7d
Deirdre Chong <xerzen@live.com>
2023-02-18 20:46:01 -0600

    feat: add http request logger

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@ import lambda from '@vendia/serverless-express';
 import express from 'express';
 
 import { PORT } from './config/server';
+import logger from './lib/morgan';
 import routes from './routes';
 
-const server = express();
+const app = express();
 
-server.use('/', routes);
+app.use(logger);
+
+app.use('/', routes);
 
 if (!process.env.AWS_LAMBDA_RUNTIME_API) {
-  server.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Starting server on port ${PORT}...`);
   });
 }
 
-export const handler = lambda({ app: server });
+export const handler = lambda({ app });
